ede0d6adbcc65476f6bcbb262e0ae7e26f08cf13
Douglas Grund <douglasgrund@yahoo.com.br>
2024-06-03 21:00:11 -0300

    feat: adiciona variáveis first_str e last_str

--- Prefix.rb
+++ Prefix.rb
@@ -3,9 +3,11 @@
 # Write a function to find the longest common prefix string
 # amongst an array of strings.
 
-If there is no common prefix, return an empty string "".
 def longest_common_prefix(strs)
   return "" if strs.empty?
 
   strs.sort!
+
+  first_str = strs.first
+  last_str = strs.last
 end
