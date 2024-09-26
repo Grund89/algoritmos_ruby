819f1d6480bc0813a40589357c04593366b35527
Douglas Grund <douglasgrund@yahoo.com.br>
2024-09-26 00:19:49 -0300

    feat: implementar função de pesquisa linear para encontrar elemento em um array

--- algorithm_linear.rb
+++ algorithm_linear.rb
@@ -7,3 +7,24 @@
 # Move: Otherwise, move to the next element in the collection.
 # Repeat: Repeat steps 2-4 until we have reached the end of collection.
 # Not found: If the end of the collection is reached without finding the desired element, return that the desired element is not in the array.
+
+# Função de pesquisa linear
+def pesquisa_linear(arr, chave)
+  arr.each_with_index do |elemento, indice|
+    if elemento == chave
+      return "Elemento encontrado no índice #{indice}"
+    end
+  end
+  return "Nenhuma correspondência encontrada"
+end
+
+# Array fornecido
+sequencia = [10, 50, 30, 70, 80, 60, 20, 90, 40]
+
+# Solicitando a chave de busca
+print "Digite o número que deseja buscar: "
+chave = gets.chomp.to_i
+
+# Executando a pesquisa linear
+resultado = pesquisa_linear(sequencia, chave)
+puts resultado
